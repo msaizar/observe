6a5e0adb6eeac98ca999c9f729854d03e1938a8b
msaizar <saizar.martin@gmail.com>
2018-12-09 22:39:40 -0300

    wait for js to add fade to remove, limit script to articles

--- observe.js
+++ observe.js
@@ -3,5 +3,16 @@ window.localStorage.clear();
 var element = document.querySelector('div.mensaje_paywall2');
 element.parentElement.removeChild(element);
 
-var element = document.querySelector('div.cuerpo');
-element.classList.remove("fade");
+window.addEventListener("load", main, false);
+
+function main(evt) {
+    var timer = setInterval(fade_check, 111);
+
+    function fade_check() {
+        if (document.querySelector("div.fade")) {
+            clearInterval(timer);
+            var element = document.querySelector('div.fade');
+            element.classList.remove('fade');
+        }
+    }
+}
